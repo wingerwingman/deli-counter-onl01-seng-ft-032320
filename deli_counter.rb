8ab22020074a0c81b45edc4b9249fecdc950b6e4
Justin Winger <justinwinger4@gmail.com>
2020-03-25 16:36:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
-  # if person == 0 
+  if katz_deli == 0 
     puts "The line is currently empty."
-  # else 
-  #   return katz_deli + "katz_deli[-1]"
+  else 
+    return katz_deli + "katz_deli[-1]"
 end 
