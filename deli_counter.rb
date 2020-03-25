ee4944f50c130122f3a327c1efd1d084d61b457f
Justin Winger <justinwinger4@gmail.com>
2020-03-25 16:30:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-deli_counter(deli_counter)
-  if deli == 0 
+deli_counter(person)
+  # if person == 0 
     return "The line is currently empty"
-  else 
-    return katz_deli + "katz_deli[-1]"
+  # else 
+  #   return katz_deli + "katz_deli[-1]"
 end 
